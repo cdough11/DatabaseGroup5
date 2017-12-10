894c4addcf7bcbc92af2f6ba8c5e4fa4fec5e14f
Carrie Dougher <cdough11@students.towson.edu>
2017-12-10 16:52:11 -0500

    Chnaged GUI layout for editSetlist

--- FinalProject/src/EditSetlist.java
+++ FinalProject/src/EditSetlist.java
@@ -13,6 +13,9 @@ public class EditSetlist {
 	private JTextField textField;
 	private JTextField textField_1;
 	private JTextField textField_2;
+	private JButton btnAddToSetlist;
+	private JButton btnDeleteFromSetlist;
+	private JButton btnViewSetlist;
 
 	/**
 	 * Launch the application.
@@ -46,17 +49,17 @@ public class EditSetlist {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JLabel lblBandName = new JLabel("Band Name");
+		JLabel lblBandName = new JLabel("performance_id");
 		lblBandName.setBounds(10, 11, 87, 14);
 		frame.getContentPane().add(lblBandName);
 		
 		textField = new JTextField();
-		textField.setBounds(10, 36, 367, 20);
+		textField.setBounds(10, 36, 142, 20);
 		frame.getContentPane().add(textField);
 		textField.setColumns(10);
 		
-		JLabel lblDateOfPerformace = new JLabel("Date of Performace (MM/DD/YYYY)");
-		lblDateOfPerformace.setBounds(10, 67, 220, 14);
+		JLabel lblDateOfPerformace = new JLabel("song_id");
+		lblDateOfPerformace.setBounds(10, 67, 51, 14);
 		frame.getContentPane().add(lblDateOfPerformace);
 		
 		textField_1 = new JTextField();
@@ -64,12 +67,8 @@ public class EditSetlist {
 		frame.getContentPane().add(textField_1);
 		textField_1.setColumns(10);
 		
-		JLabel lblSetlist = new JLabel("Setlist");
-		lblSetlist.setBounds(10, 123, 46, 14);
-		frame.getContentPane().add(lblSetlist);
-		
 		textField_2 = new JTextField();
-		textField_2.setBounds(10, 148, 414, 68);
+		textField_2.setBounds(10, 130, 414, 86);
 		frame.getContentPane().add(textField_2);
 		textField_2.setColumns(10);
 		
@@ -83,6 +82,18 @@ public class EditSetlist {
 		});
 		saveBtn.setBounds(162, 227, 110, 23);
 		frame.getContentPane().add(saveBtn);
+		
+		btnAddToSetlist = new JButton("Add To Setlist");
+		btnAddToSetlist.setBounds(269, 33, 110, 23);
+		frame.getContentPane().add(btnAddToSetlist);
+		
+		btnDeleteFromSetlist = new JButton("Delete From Setlist");
+		btnDeleteFromSetlist.setBounds(269, 63, 123, 23);
+		frame.getContentPane().add(btnDeleteFromSetlist);
+		
+		btnViewSetlist = new JButton("View Setlist");
+		btnViewSetlist.setBounds(269, 91, 89, 23);
+		frame.getContentPane().add(btnViewSetlist);
 	}
 	
 	public void makeVisible() {
