b70f56150b8beace045348ed9cebb10b335cd8b1
RML94 <rlee14@students.towson.edu>
2017-12-12 20:09:39 -0500

    Removed the code that exits out all screens

---
+++ FinalProject/ModeratorGUI.java
@@ -0,0 +1,198 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+import songsDAC.Band;
+import songsDAC.User;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.List;
+
+import javax.swing.JTextPane;
+
+public class ModeratorGUI {
+
+	private JFrame frame;
+	private JTextField bandNameTF;
+	private JTextField txtEnterFormationDate;
+	private JTextField txtEnterBreakupDate;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ModeratorGUI window = new ModeratorGUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public ModeratorGUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 550, 413);
+		frame.getContentPane().setLayout(null);
+		
+		bandNameTF = new JTextField();
+		bandNameTF.setText("Enter Band");
+		bandNameTF.setBounds(18, 11, 131, 20);
+		frame.getContentPane().add(bandNameTF);
+		bandNameTF.setColumns(10);
+		
+		JTextPane textPane = new JTextPane();
+		textPane.setBounds(18, 121, 414, 137);
+		frame.getContentPane().add(textPane);
+		
+		JButton addBand = new JButton("Add Band");
+		addBand.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				String bandName = bandNameTF.getText();
+				String formationDate = txtEnterFormationDate.getText();
+				String breakupDate = txtEnterBreakupDate.getText();
+				Band.addBandToDB(bandName, formationDate, breakupDate);
+			}
+		});
+		addBand.setBounds(10, 51, 89, 23);
+		frame.getContentPane().add(addBand);
+		
+		JButton deleteBand = new JButton("Delete Band");
+		deleteBand.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				String bandName = bandNameTF.getText();
+				Band.deleteBandFromDB(bandName);
+			}
+		});
+		deleteBand.setBounds(130, 51, 105, 23);
+		frame.getContentPane().add(deleteBand);
+		
+		JButton editSetlist = new JButton("Edit Setlist");
+		editSetlist.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				EditSetlist window = new EditSetlist();
+				window.makeVisible();
+			}
+		});
+		editSetlist.setBounds(166, 270, 116, 23);
+		frame.getContentPane().add(editSetlist);
+		
+		JButton manageComments = new JButton("Manage Comments");
+		manageComments.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				ManageComments window = new ManageComments();
+				window.makeVisible();
+			}
+		});
+		manageComments.setBounds(231, 305, 181, 23);
+		frame.getContentPane().add(manageComments);
+		
+		JButton btnAddToFavorites = new JButton("Add to Favorites");
+		btnAddToFavorites.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				Band band = new Band(bandNameTF.getText());
+				GUIlogin.loggedInUser.addNewFavoriteBand(band);
+			}
+		});
+		btnAddToFavorites.setBounds(10, 86, 139, 23);
+		frame.getContentPane().add(btnAddToFavorites);
+		
+		JButton btnDeleteFromFavorites = new JButton("Delete from Favorites");
+		btnDeleteFromFavorites.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				Band band = new Band(bandNameTF.getText());
+				GUIlogin.loggedInUser.deleteFavoriteBand(band);
+			}
+		});
+		btnDeleteFromFavorites.setBounds(166, 86, 169, 23);
+		frame.getContentPane().add(btnDeleteFromFavorites);
+		
+		JButton showFavorites = new JButton("Show Favorites");
+		showFavorites.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				List<Band> favoriteBands = GUIlogin.loggedInUser.favoriteBands;
+				String favBandNames = "";
+				for(Band band: favoriteBands) {
+					favBandNames += band.name + "\n";
+				}
+				textPane.setText(favBandNames);
+			}
+		});
+		showFavorites.setBounds(368, 86, 124, 23);
+		frame.getContentPane().add(showFavorites);
+		
+		JButton addPerformaceBtn = new JButton("Add Performance");
+		addPerformaceBtn.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				AddPerformance window = new AddPerformance();
+				window.makeVisible();
+			}
+		});
+		addPerformaceBtn.setBounds(10, 270, 139, 23);
+		frame.getContentPane().add(addPerformaceBtn);
+		
+		JButton perfCommentsBtn = new JButton("View Performace Comments");
+		perfCommentsBtn.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				ViewComments window = new ViewComments();
+				window.makeVisible();
+			}
+		});
+		perfCommentsBtn.setBounds(10, 305, 209, 23);
+		frame.getContentPane().add(perfCommentsBtn);
+		
+		JButton btnDeleteAccount = new JButton("Delete Account");
+		btnDeleteAccount.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				User.deleteFromDB(GUIlogin.loggedInUser.userID);
+				GUIlogin window = new GUIlogin();
+				window.makeVisible();
+			}
+		});
+		btnDeleteAccount.setBounds(10, 339, 139, 23);
+		frame.getContentPane().add(btnDeleteAccount);
+		
+		txtEnterFormationDate = new JTextField();
+		txtEnterFormationDate.setText("Enter Formation Date");
+		txtEnterFormationDate.setBounds(159, 11, 123, 20);
+		frame.getContentPane().add(txtEnterFormationDate);
+		txtEnterFormationDate.setColumns(10);
+		
+		txtEnterBreakupDate = new JTextField();
+		txtEnterBreakupDate.setText("Enter Breakup Date");
+		txtEnterBreakupDate.setBounds(292, 11, 120, 20);
+		frame.getContentPane().add(txtEnterBreakupDate);
+		txtEnterBreakupDate.setColumns(10);
+	}
+	
+	public void makeVisible() {
+		this.frame.setVisible(true);
+	}
+}
---
+++ FinalProject/RegisterUser.java
@@ -0,0 +1,117 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+import songsDAC.User;
+
+import javax.swing.JLabel;
+import javax.swing.JCheckBox;
+import javax.swing.JButton;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class RegisterUser {
+
+	private JFrame frame;
+	private JTextField firstName;
+	private JTextField lastName;
+	private JTextField age;
+	private JTextField email;
+	private JTextField password;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					RegisterUser window = new RegisterUser();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public RegisterUser() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.getContentPane().setLayout(null);
+		
+		firstName = new JTextField();
+		firstName.setBounds(204, 11, 86, 20);
+		frame.getContentPane().add(firstName);
+		firstName.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("First Name");
+		lblNewLabel.setBounds(136, 14, 58, 14);
+		frame.getContentPane().add(lblNewLabel);
+		
+		lastName = new JTextField();
+		lastName.setBounds(204, 40, 86, 20);
+		frame.getContentPane().add(lastName);
+		lastName.setColumns(10);
+		
+		JLabel lblLastName = new JLabel("Last Name");
+		lblLastName.setBounds(136, 43, 56, 14);
+		frame.getContentPane().add(lblLastName);
+		
+		age = new JTextField();
+		age.setBounds(204, 71, 86, 20);
+		frame.getContentPane().add(age);
+		age.setColumns(10);
+		
+		JLabel lblAge = new JLabel("Age");
+		lblAge.setBounds(136, 74, 46, 14);
+		frame.getContentPane().add(lblAge);
+		
+		email = new JTextField();
+		email.setBounds(204, 101, 86, 20);
+		frame.getContentPane().add(email);
+		email.setColumns(10);
+		
+		JLabel lblEmail = new JLabel("Email");
+		lblEmail.setBounds(136, 104, 46, 14);
+		frame.getContentPane().add(lblEmail);
+		
+		password = new JTextField();
+		password.setBounds(204, 132, 86, 20);
+		frame.getContentPane().add(password);
+		password.setColumns(10);
+		
+		JLabel lblPassword = new JLabel("Password");
+		lblPassword.setBounds(136, 135, 46, 14);
+		frame.getContentPane().add(lblPassword);
+		
+		JCheckBox chckbxModerator = new JCheckBox("Moderator?");
+		chckbxModerator.setBounds(160, 169, 97, 23);
+		frame.getContentPane().add(chckbxModerator);
+		
+		JButton btnSubmit = new JButton("Submit");
+		btnSubmit.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				User.addUserToDB(firstName.getText(), lastName.getText(), Integer.parseInt(age.getText()), email.getText(), chckbxModerator.isSelected(), password.getText());
+			}
+		});
+		btnSubmit.setBounds(168, 211, 89, 23);
+		frame.getContentPane().add(btnSubmit);
+	}
+	
+	public void makeVisible() {
+		this.frame.setVisible(true);
+	}
+}
